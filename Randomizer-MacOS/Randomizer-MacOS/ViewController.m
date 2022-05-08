a2cc42984d315b6c5e3568d73cd558720d37901b
davoleo <dav@davoleo.net>
2022-05-08 15:26:16 +0200

    Basic functionality: done

---
+++ Randomizer-MacOS/Randomizer-MacOS/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Randomizer-MacOS
+//
+//  Created by Leo Dav on 08/05/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController()
+
+@property (weak) IBOutlet NSNumberFormatter* formatter;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.formatter.allowsFloats = NO;
+    self.maxBound = 1;
+}
+
+- (IBAction)generate:(NSButton *)sender {
+    unsigned int generated = arc4random_uniform(self.maxBound) + 1;
+    self.labelGenerated.integerValue = generated;
+}
+
+
+@end
---
+++ Randomizer-MacOS/Randomizer-MacOS/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Randomizer-MacOS
+//
+//  Created by Leo Dav on 08/05/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
